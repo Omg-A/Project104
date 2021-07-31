7b293607b67a297060c894d823cb4582b41c9c18
Omg-A <67595078+Omg-A@users.noreply.github.com>
2021-07-31 15:32:36 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -20,3 +20,18 @@ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models
 function modelLoaded (){
     console.log("model loaded");
 }
+
+function check(){
+    img = document.getElementById("capture_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
