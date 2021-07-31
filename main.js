bd2bd4ce9ffa78399b4a3af89736767d9633d1c4
Omg-A <67595078+Omg-A@users.noreply.github.com>
2021-07-31 14:55:22 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,24 +15,8 @@ function take_snapshot(){
 }
 
 console.log('ml5version', ml5.version);
-
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/btHUGtTe2/model.json', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/zHOTecYXr/model.json', modelLoaded);
 
 function modelLoaded (){
     console.log("model loaded");
-}
-
-function check(){
-    img = document.getElementById("capture_image");
-    classifier.classify(img, gotResult);
-}
-
-function gotResult(error, results){
-    if(error){
-        console.log(error);
-    }else{
-        console.log(results);
-        document.getElementById("result_object_name").innerHTML = results[0].label;
-        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
-    }
 }
